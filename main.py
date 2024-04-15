5e47c5407266885549274dc688b9cc654e3ec2ea
Adam Lear <adamlear12@gmail.com>
2024-04-14 21:35:49 -0500

    reorganize file structure

---
+++ main.py
@@ -0,0 +1,4 @@
+from track import track
+
+if __name__ == "__main__":
+    track()
--- track.py
+++ track.py
@@ -6,10 +6,10 @@ from send_data import send_data
 
 def track():
     # Load the YOLOv8 model
-    model = YOLO("runs/detect/yolov8n_v8_50e18/weights/best.pt")
+    model = YOLO("yolo-model/runs/detect/yolov8n_v8_50e18/weights/best.pt")
 
     # cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("inference_data/pothole_video.mp4")
+    cap = cv2.VideoCapture("yolo-model/inference_data/pothole_video.mp4")
 
     max_id = 0
 
@@ -19,10 +19,11 @@ def track():
         if success:
             results = model.track(frame, persist=True)
 
-            for id in results[0].boxes.id:
-                if id > max_id:
-                    # send_data
-                    max_id = id
+            if results[0].boxes.id is not None:
+                for id in results[0].boxes.id:
+                    if id > max_id:
+                        # send_data
+                        max_id = id
 
             annotated_frame = results[0].plot()
 
